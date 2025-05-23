c203f25bd43b59356083c13729d95159038ff219
conghung2302 <tranconghung2023@gmail.com>
2025-05-23 11:04:20 +0700

    add run.sh main.c

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+gcc -o a "$1" 
+./a
+#
+
+#
+# echo "Script name: $0"
+# echo "First argument: $1"
+# echo "Second argument: $2"
+# echo "Total arguments: $#"
+
+# Example usage: ./script.sh arg1 arg2
